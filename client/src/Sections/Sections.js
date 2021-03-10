4da1b58343824fe68ce45c23c93c273bae9f32a2
Yosabet <a13yosabet@gmail.com>
2021-03-10 12:36:18 -0800

    Updated sections list and created new js called sections form

--- client/src/Sections/Sections.js
+++ client/src/Sections/Sections.js
@@ -1,6 +1,6 @@
 import { Route, Switch, useRouteMatch } from "react-router-dom";
-
 import SectionsList from './SectionsList';
+import SectionForm from './SectionForm';
 
 function Sections(){
     const {path} = useRouteMatch();
@@ -11,7 +11,11 @@ function Sections(){
                 <SectionsList />
            </Route>
            <Route path ={`${path}/new`}>
-            
+            <SectionForm />
+           </Route>
+           
+           <Route path ={`${path}/:id/edit`}>
+            <SectionForm />
            </Route>
        </Switch>
     );
